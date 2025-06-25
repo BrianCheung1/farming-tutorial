208cb29a73fbf0d5f76b7e7e6aea2f5428fcb613
Brian Cheung <44447718+BrianCheung1@users.noreply.github.com>
2025-06-24 22:06:29 -0400

    feat: added more collectables using reusable component

--- scenes/characters/player/player.gd
+++ scenes/characters/player/player.gd
@@ -4,11 +4,15 @@ extends CharacterBody2D
 @onready var hit_component = $HitComponent
 
 @export var current_tool: DataTypes.Tools = DataTypes.Tools.None
+@export var speed: float = 50.0
 
 var player_direction:Vector2
+var walk_state
 
 
 func _ready()->void:
+	walk_state = $StateMachine/Walk
+	walk_state.speed = speed
 	ToolManager.tool_selected.connect(on_tool_selected)
 
 func on_tool_selected(tool:DataTypes.Tools) -> void:
